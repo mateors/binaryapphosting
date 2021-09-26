d42c766e4d1485fe7326a18ff7f7102597fcf14b
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-09-26 10:56:05 +0600

    golang command pipeline example

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"runtime"
@@ -243,8 +244,20 @@ func main() {
 	// fmt.Println(ipaddr, len(ipaddr), err)
 
 	//cat /etc/passwd | cut -d: -f 1
-	bs, err := execCommand("bash -c cat /etc/passwd | cut -d : -f 1")
-	fmt.Println(string(bs), err)
+
+	// bs, err := execCommand("bash -c cat /etc/passwd") //cut -d : -f 1
+	// fmt.Println(string(bs), err)
+
+	//golang command pipeline example
+	var b bytes.Buffer
+	if err := Execute(&b,
+		exec.Command("cat", "/etc/passwd"),
+		exec.Command("cut", "-d", ":", "-f", "1"),
+		exec.Command("grep", "rassel"),
+	); err != nil {
+		log.Fatalln(err)
+	}
+	io.Copy(os.Stdout, &b)
 
 }
 
@@ -344,4 +357,44 @@ func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
 			return out, err
 		}
 	}
+}
+
+func Execute(output_buffer *bytes.Buffer, stack ...*exec.Cmd) (err error) {
+	var error_buffer bytes.Buffer
+	pipe_stack := make([]*io.PipeWriter, len(stack)-1)
+	i := 0
+	for ; i < len(stack)-1; i++ {
+		stdin_pipe, stdout_pipe := io.Pipe()
+		stack[i].Stdout = stdout_pipe
+		stack[i].Stderr = &error_buffer
+		stack[i+1].Stdin = stdin_pipe
+		pipe_stack[i] = stdout_pipe
+	}
+	stack[i].Stdout = output_buffer
+	stack[i].Stderr = &error_buffer
+
+	if err := call(stack, pipe_stack); err != nil {
+		log.Fatalln(string(error_buffer.Bytes()), err)
+	}
+	return err
+}
+
+func call(stack []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
+	if stack[0].Process == nil {
+		if err = stack[0].Start(); err != nil {
+			return err
+		}
+	}
+	if len(stack) > 1 {
+		if err = stack[1].Start(); err != nil {
+			return err
+		}
+		defer func() {
+			if err == nil {
+				pipes[0].Close()
+				err = call(stack[1:], pipes[1:])
+			}
+		}()
+	}
+	return stack[0].Wait()
 }
